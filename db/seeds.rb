34de080426f3d4bbc63afb04f5f2bed3892ed10c
kelven <kelvenstone@163.com>
2017-05-13 22:03:14 +0800

    modify the seed file for user.rb presence

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 puts "这个种子会自动建立一个admin账号"
 
-create_account = User.create([email: '53053622@qq.com', password: '123456', password_confirmation: '123456', is_admin: 'true'])
+create_account = User.create([email: '53053622@qq.com', password: '123456', password_confirmation: '123456', is_admin: 'true', name: '郭凯明', school: '浙江', qq_number: '53053622', phone_number:'13386660798'])
 puts "Admin account created."
